f4649f506efb916d65ca39c8d7dfde468cf80bf1
Nikola Spasojevic <nikola.t.spasojevic@gmail.com>
2018-11-24 11:23:04 +0100

    Added modularity - included player setup and board setup as separate modules

--- connect_four.py
+++ connect_four.py
@@ -1,88 +1,9 @@
-class ConnectFour:
-
-	def __init__(self, height = 5, width = 5):
-		self.height = height
-		self.width = width
-		self.board = [[0]*width for _ in range(height)] # initiliase board size to height x width dimensions
-		self._game_over = False
-		self._turn = False
-		self._DEFAULT_COLOUR_1 = 'RED'
-		self._DEFAULT_COLOUR_2 = 'GREEN'
-		self.players = {0: ('Player 1', self._DEFAULT_COLOUR_1), 1: ('Player 2', self._DEFAULT_COLOUR_2)} # Dictionary corresponding to players' names and colours
-
-	def print_board(self):
-		for row in self.board:
-			print(row)
-		print()
-
-	def choose_colours(self):
-		for k, v in self.players.items():
-			player_colour = input('Please input colour for {}: '.format(v[0]))
-			self.players[k] = (v[0], player_colour)
-
-	def winning_move(self):
-		pass
-		# BFS
-		# horizontal_count, vertical_count, diagonal_count
-		# 
-
-	def is_valid_location(self, col):
-		if self.board[0][col]:
-			print('No more space in current column.\n')
-			return False
-		else:
-			return True
-
-	def choose_column(self):
-		# Within this loop we impose that the player must provide a valid input (i.e. an int within the range of the game's board matrix)
-		while True:
-				player_input = input("{}'s Move: ".format(self.players[self._turn][0]))
-				try:
-					col = int(player_input)
-					if (col >= self.width or col < 0):
-						print('Out Of Bounds! Please choose a value between {} and {}!'.format(0, self.width-1))
-					else:
-						break
-				except ValueError:
-					print("{} was not a number, please use a valid input.".format(player_input))		
-		return col
-
-	def get_next_open_row(self, col):
-		if self.is_valid_location(col):
-			for r in range(self.height)[::-1]: # range(self.height-1, -1, -1)
-				if not self.board[r][col]:
-					return r
-		else:
-			return -1
-
-	def drop_disk(self, colour, col):
-		while True:
-			row = self.get_next_open_row(col)
-			if row == -1:
-				print('Please choose another column\n')
-				col = self.choose_column()
-			else:
-				break
-		self.board[row][col] = colour
-
-	def play(self):
-		# self.choose_colours()
-	
-		while not self._game_over:
-			# Ask the player whose tuen it is to choose a column - which is checked and validated
-			col = self.choose_column()
-
-			# Insert Player 1's colour in the chosen column
-			if self._turn:
-				self.drop_disk(1, col)
-
-			# Insert Player 2's colour in the chosen column
-			else:
-				self.drop_disk(2, col)
-
-			self._turn = not self._turn 	# alternate between players
-			self.print_board()	# check status of game
+from src.board_setup import BoardSetup
+from src.player_setup import PlayerSetup
 
 if __name__ == '__main__':
-	connect_four = ConnectFour()
-	connect_four.play()
+	player_setup = PlayerSetup()
+	# player_setup.run()
+	
+	connect_four_game = BoardSetup(player_setup.get_players())
+	connect_four_game.play()
---
+++ src/player_setup.py
@@ -0,0 +1,28 @@
+class PlayerSetup:
+
+	ACCEPTABLE_ANSWERS = {'y', 'yes', '1'}
+
+	def __init__(self, player_1 = ('Player 1', 'RED'), player_2 = ('Player 2', 'GREEN')):
+		self.player_1 = player_1
+		self.player_2 = player_2
+		self.run()
+
+	def choose_name_colour(self, player):
+		player_name = input('Please input name for {}: '.format(player[0]))
+		player_colour = input('\nPlease input {}\'s choice of colour: '.format(player_name))
+		print()
+		return (player_name, player_colour)
+
+	def configure_player(self):
+		self.player_1 = self.choose_name_colour(self.player_1)
+		self.player_2 = self.choose_name_colour(self.player_2)
+
+	def run(self):
+		to_configure = str(input('Do you want to configure your players?'))
+		
+		if to_configure.lower() in self.ACCEPTABLE_ANSWERS:
+			self.configure_player()
+
+	def get_players(self):
+		return (self.player_1, self.player_2) # Returns a tuple containing player 1's config at index 0 and player 2's config at index 1
+
