221712ec0f8a3ca3af5ce749fa51cebf6f836a07
Nikola Spasojevic <nikola.t.spasojevic@gmail.com>
2018-11-24 20:10:44 +0100

    Added player comparison checks (no same name or colour allowed)

--- src/player_setup.py
+++ src/player_setup.py
@@ -9,17 +9,23 @@ class PlayerSetup:
 
 	def choose_name_colour(self, player):
 		player_name = input('Please input name for {}: '.format(player[0])).strip().upper()
-		player_colour = input('\nPlease input {}\'s choice of colour: '.format(player_name)).strip().upper()
+		player_colour = input('\nPlease input {}\'s choice of colour: '.format(player_name)).strip().upper() #TODO: colour cannot be 0. there should be a set of colours to be chosen from
 		print()
 		return (player_name, player_colour)
 
 	def configure_player(self):
 		self.player_1 = self.choose_name_colour(self.player_1)
-		self.player_2 = self.choose_name_colour(self.player_2)
+		# Invoke that the players' colours and names must remain different
+		while True:
+			self.player_2 = self.choose_name_colour(self.player_2)
+			if (self.player_1[0] == self.player_2[0] or self.player_1[1] == self.player_2[1]):
+				print('Player names and colours must be different!\n\nPlease choose new Player 2 values\n')
+				self.player_2 = ('Player 2', 'GREEN') # player 2 value reset
+			else:
+				break
 
 	def run(self):
 		to_configure = str(input('Do you want to configure your players?')).strip().lower()
-		
 		if to_configure in PlayerSetup.ACCEPTABLE_ANSWERS:
 			self.configure_player()
 
