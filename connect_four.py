811771045e3f8457654dad36c492de7859280fa9
Nikola Spasojevic <nikola.t.spasojevic@gmail.com>
2018-11-24 11:25:41 +0100

    removed comments

--- connect_four.py
+++ connect_four.py
@@ -3,7 +3,5 @@ from src.player_setup import PlayerSetup
 
 if __name__ == '__main__':
 	player_setup = PlayerSetup()
-	# player_setup.run()
-	
 	connect_four_game = BoardSetup(player_setup.get_players())
 	connect_four_game.play()
